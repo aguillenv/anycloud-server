0091b35c6c8a7ee525ff5aced3e4670fd10bb19f
Alejandro Guillen <aguillenv93@gmail.com>
2021-06-02 11:37:35 +0200

    Comment ds

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import { randomBytes } from "crypto";
+// import { DS } from "anycloud";
 
 const port = 8088;
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 // 1 GB post payload limit
 app.use(express.json({ limit: "1000MB" }));
 app.get('/', (_, res) => res.send('Hello'));
+// app.get('/ds/get', (_, res) => {
+//   res.send(DS['foo']);
+// });
 app.get('/health', (_, res) => res.send('ok'));
 app.post("/fibo", (req, res, next) => {
   const fibo = (n: number) => {
