bd8451d2f3e3b4b00cc37b76453de975622b39b9
Alejandro Guillen <aguillenv93@gmail.com>
2021-06-03 12:39:37 +0200

    update anycloud version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
 import { randomBytes } from "crypto";
-import { DS } from "anycloud-test8";
+import { DS } from "anycloud-test9";
 
 const port = 8088;
 const app = express();
