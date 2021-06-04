b36024ad4b7b55a8950b6910a2a88275320a8e1c
Alejandro Guillen <aguillenv93@gmail.com>
2021-06-04 17:12:30 +0200

    upate

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
 import { randomBytes } from "crypto";
-import { DS } from "anycloud21";
+import { DS } from "anycloud22";
 
 const port = 8088;
 const app = express();
